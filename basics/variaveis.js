61861255a3c0ff3a6f8caaf2444610441678155b
Lecio <lecio.vilela@yahoo.com>
2021-03-24 17:04:10 -0300

    update repository

---
+++ basics/variaveis.js
@@ -0,0 +1,12 @@
+var a = 10
+let b = 20
+const c = 30
+
+let vetor = [1, 2, 3, 4]
+console.log(vetor[2])
+
+let funcao = function () {
+    console.log('de dentro da funcao')
+}
+let funcaoCopia = funcao
+funcaoCopia(console.log)
