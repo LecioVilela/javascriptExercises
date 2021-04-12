6c667cb4229f4ce7898f8921973d050933811c0d
LecioVilela <lecio.vilela@yahoo.com>
2021-04-12 15:10:01 -0300

    upload new exercises

---
+++ exercises-02/strings.js
@@ -0,0 +1,12 @@
+const name = 'Lecio Vilela'
+const num1 = 10
+console.log(name.split(' ').join(' <==> '))
+
+const opa = `
+asdf
+Nome: ${name}
+1+1 = ${num1+1}`
+
+const outra = name + ' ' + opa
+
+console.log(opa)
